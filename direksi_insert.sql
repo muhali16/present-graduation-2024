df75e7936a8b89a13ca6e5b6a0fc074d5127d06e
muhali16 <muhammadali55214@gmail.com>
2024-06-11 10:42:41 +0700

    add insert direksi sql

---
+++ direksi_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO `siswa_lulus` (`unit_sekolah`, `departemen`, `nis`, `nama`, `kelas`, `ayah`, `ibu`, `wali`, `waayah`, `waibu`, `wawali`, `hadir`, `aktif`, `timestamp_hadir`) VALUES
+('Sekolah Kak Seto', 'Direksi', 1000000000, 'Prof. Dr. H. Seto Mulyadi, S.Psi., M.Si., Psikolog.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1200000000, 'Januar Malik Djajasartika, S.E., M.E.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1300000000, 'Dimas Ramdani Triputra, S.E., M.M., M.I.Kom.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1400000000, 'Nur Annisa Muharromah, S.E., M.Ak.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1500000000, 'Fery Citra Febriyanto, S.E., M.M.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1600000000, 'Nurhuda Abdillah, M.Pd.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1700000000, 'Iin Imronah Amelia, S.Pd.I.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1800000000, 'Ade Rachmat Fikri, M.Pd.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18'),
+('Sekolah Kak Seto', 'Direksi', 1900000000, 'Adhityo Haryadi, M.Pd.', 'SEKOLAH KAK SETO', '0', '0', '0', 0, 0, 0, 0, 1, '2024-06-06 07:09:18');
